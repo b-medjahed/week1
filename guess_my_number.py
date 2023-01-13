87baa0fa16ad111c4308b44dc0dd67caf5b31f0a
Bachir Medjahed <medjahed_bachir@yahoo.fr>
2023-01-13 15:20:52 +0000

    included the check if the input is an integer

--- guess_my_number.py
+++ guess_my_number.py
@@ -1,6 +1,6 @@
 import random
 my_number=random.randint(1,100)
-print("\nAre you up for Guess The Number Challenge?\n")
+print("\nAre you up for Guess The Number Challenge? You have 7 attempts only?\n")
 
 choice=input("\nType in y for yes Or n for no: ")
 
@@ -9,17 +9,29 @@ while choice!="y" and choice!="n":
 
 attempt=1
 
+
 while True:
+  
+  if choice=="n":
+   print("\nsee you next time!\n")
+   break
+  
   if attempt>7 :
     print("\nSorry! You have used up your seven attempts. ","The answer is ", my_number,"!\n")
     break
    
-  user_number=int(input("\nPlease enter a number between 1 and 100: "))
-   
-  if user_number==my_number:
+  user_number=input("\nPlease enter a number between 1 and 100: ")
+  check=user_number
+  try:
+    check=int(user_number)
+  except ValueError:
+    print("Please enter a numerical value!")
+    continue 
+
+  if check==my_number:
      print("\nWell done. You guessed right!","You used ", attempt," attempt(s)\n")
      break
-  if my_number>user_number:
+  if my_number>check:
      print("\nIncorrect guess. Your number is low!","You used ", attempt," attempt(s)")
      attempt+=1
   else:
